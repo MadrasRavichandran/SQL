3868bb9a470f4e9879a8a6af9bd1e51897e4372b
MadrasRavichandran <lokeshkumar.mr@outlook.com>
2016-09-12 00:26:59 -0700

    Delete Book_List.sql

--- Book_List.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE book_list (id INTEGER PRIMARY KEY, name TEXT, rating REAL);
-INSERT INTO book_list VALUES (1,'Introductoin to Psycology',7.2);
-INSERT INTO book_list VALUES (2,' In Search of Lost Time by Marcel Proust',8);
-INSERT INTO book_list VALUES (3,'When to buy', 9.12);
-INSERT INTO book_list VALUES (4,'When to Sell',6.84);
-INSERT INTO book_list VALUES (5,'The doctrine of Lapse',3.42);
-INSERT INTO book_list VALUES (6,'Hail Ceaser',5.67);
-SELECT * FROM book_list ORDER BY rating DESC;
